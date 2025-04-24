883b951f32468edf5f1c51e43ede6d52ab90b6f2
SanjiPk <zero.0102003@gmail.com>
2025-04-24 20:55:34 +0330

    Update FileManager.java

--- src/editor/FileManager.java
+++ src/editor/FileManager.java
@@ -1,29 +1,78 @@
 package editor;
 
 import javax.swing.*;
+import java.io.*;
 
 public class FileManager {
     public static void openFile(TextEditor textEditor, JTextArea textArea) {
         JFileChooser fileChooser = new JFileChooser();
         if (fileChooser.showOpenDialog(textEditor) == JFileChooser.APPROVE_OPTION) {
-            //File file = fileChooser.getSelectedFile();
-            //TODO
+            File file = fileChooser.getSelectedFile();
+            File chosenFile = fileChooser.getSelectedFile();
+            if (chosenFile != null && chosenFile.exists()) {
+                try {
+                    FileReader fileReader = new FileReader(chosenFile);
+                    BufferedReader bufferedReader = new BufferedReader(fileReader);
+
+                    StringBuilder fileContent = new StringBuilder();
+                    String currentLine;
+                    while ((currentLine = bufferedReader.readLine()) != null) {
+                        fileContent.append(currentLine).append(System.lineSeparator());
+                    }
+
+                    textArea.setText(fileContent.toString());
+                    textEditor.currentFile = chosenFile;
+                    textEditor.setTitle("Simple Text Editor - " + chosenFile.getName());
+
+                    bufferedReader.close();
+                } catch (IOException ioEx) {
+                    JOptionPane.showMessageDialog(
+                            textEditor,
+                            "Could not open file: " + ioEx.getMessage(),
+                            "File Error",
+                            JOptionPane.ERROR_MESSAGE
+                    );
+                }
+            }
+
+
         }
     }
 
     public static void saveFile(TextEditor textEditor, JTextArea textArea) {
-        if (textEditor.currentFile == null){
-            JFileChooser fileChooser = new JFileChooser();
-            if (fileChooser.showSaveDialog(textEditor) == JFileChooser.APPROVE_OPTION) {
-                //TODO
+        File targetFile = textEditor.currentFile;
+
+        if (targetFile == null) {
+            JFileChooser chooser = new JFileChooser();
+            int userChoice = chooser.showSaveDialog(textEditor);
+
+            if (userChoice == JFileChooser.APPROVE_OPTION) {
+                targetFile = chooser.getSelectedFile();
+            } else {
+                return; // User canceled save dialog
             }
         }
-        else{
-            //TODO
+
+        try {
+            FileWriter fw = new FileWriter(targetFile);
+            BufferedWriter bw = new BufferedWriter(fw);
+            bw.write(textArea.getText());
+            bw.close();
+
+            textEditor.currentFile = targetFile;
+            textEditor.setTitle("Simple Text Editor - " + targetFile.getName());
+        } catch (IOException ex) {
+            JOptionPane.showMessageDialog(
+                    textEditor,
+                    "Failed to save file: " + ex.getMessage(),
+                    "Save Error",
+                    JOptionPane.ERROR_MESSAGE
+            );
         }
     }
 
     public static void newFile(TextEditor textEditor, JTextArea textArea) {
-        //TODO
-    }
+        textArea.setText("");
+        textEditor.currentFile = null;
+        textEditor.setTitle("Simple Text Editor");    }
 }
